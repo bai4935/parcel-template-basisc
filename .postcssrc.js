52a054cce4d10fb74e7dc91c48dd2307c30306e5
bai4935 <bai4935@gmail.com>
2023-08-10 14:51:50 +0900

    Create progect

---
+++ .postcssrc.js
@@ -0,0 +1,19 @@
+const autoprefixer =  require('autoprefixer')
+// import autoprefixer from 'autoprefixer'
+
+module.exports = {
+    Plugins: [
+        autoprefixer
+    ]
+}
+// exports = {
+//   Plugins: [
+//     autoprefixer
+//   ]
+// }
+
+/*module.exports = {
+    Plugins: [
+        require('autoprefixer')
+    ]
+}*/
